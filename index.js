68392b93386ad73c5d4da48c97a428a5176baa66
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-25 11:55:40 -0600

    commit tree 6a2c3368817c2369deaaaf1b82de49c52d0ff3e6

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) { return 'This one is on me!';}
+  else if (value > 400 && value <= 2000) {return 'That will be twenty bucks.';}
+  else if (value > 2000 && value <= 2500) {return 'I will gladly take your thirty bucks.'}
+  else return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const ans = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+  return ans;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    
+    case 'not as generous':
+      return 'Thank you.';
+
+    default:
+      return 'Bye.';
+  }
 }
